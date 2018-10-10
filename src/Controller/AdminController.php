12ed4d6c0af2a7c32d2237c89533eb31da97c312
Dench8833 <chbksrv@gmail.com>
2018-10-11 00:18:13 +0300

    11.10.18 00:17 добавлена соната админ

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -10,16 +10,6 @@ use Symfony\Component\Routing\Annotation\Route;
 class AdminController extends AbstractController
 {
     /**
-     * @Route("/admin", name="admin")
-     */
-    public function index()
-    {
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-        ]);
-    }
-
-    /**
      * @Route("/news/{slug}")
      */
     public function show($slug)
